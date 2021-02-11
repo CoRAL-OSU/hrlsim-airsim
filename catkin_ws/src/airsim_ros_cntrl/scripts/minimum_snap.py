2d611dfeae562fe3cf54e8a95077a2c08daea26b
collinthornton <collin.thornton@okstate.edu>
2021-02-11 02:46:03 -0600

    ported minimum snap to python

--- catkin_ws/src/airsim_ros_cntrl/scripts/minimum_snap.py
+++ catkin_ws/src/airsim_ros_cntrl/scripts/minimum_snap.py
@@ -1,14 +1,22 @@
 #! /usr/bin/python2
 
 import time
-import multiprocessing as mp
 import numpy as np
 import math
 import sys
 
-import airsim
-import slycot
-import control
+import matplotlib.pyplot as plt
+
+
+
+class DesiredState:
+    def __init__(self):
+        self.pos = np.zeros((3,1))
+        self.vel = np.zeros((3,1))
+        self.acc = np.zeros((3,1))
+        self.yaw = 0
+        self.yawdot = 0
+
 
 
 class MinimumSnap:
@@ -18,7 +26,7 @@ class MinimumSnap:
         
         d = waypoints[:,1:] - waypoints[:,0:-1]
         self.d0 = 2*np.sqrt(d[0,:]*d[0,:] + d[1,:]*d[1,:] + d[2,:]*d[2,:])
-        self.traj_time = np.array([0, np.cumsum(self.d0)])
+        self.traj_time = np.append(0, np.cumsum(self.d0))
         self.waypoints0 = waypoints
 
         N = np.size(waypoints, 1)-1
@@ -55,10 +63,10 @@ class MinimumSnap:
         A = np.zeros((8*N,8*N))
         b = np.zeros((8*N, 3))
 
-        for i in range(1,N):
+        for i in range(1,N+1):
 
             A[(i-1)*8, np.arange(0,8)+(i-1)*8] = head_c[0,:]
-            b[(i-1)*8, :] = waypoints[:,i].T
+            b[(i-1)*8, :] = waypoints[:,i-1].T
 
             A[(i-1)*8+1, np.arange(0,8)+(i-1)*8] = self.p_c[0,:]
             b[(i-1)*8+1, :] = waypoints[:,i].T
@@ -79,18 +87,65 @@ class MinimumSnap:
         A[8*N-2, np.arange(0,8)+8*(N-1)] = self.p_c[2,:]
         A[8*N-1, np.arange(0,8)+8*(N-1)] = self.p_c[3,:]
 
-        np.savetxt("A.csv", A, delimiter=",")
 
-        x1 = np.linalg.inv(A)*b[:,0]    
-        x2 = np.linalg.inv(A)*b[:,1]
-        x3 = np.linalg.inv(A)*b[:,2]
+        x1 = np.matmul(np.linalg.inv(A), b[:,0])    
+        x2 = np.matmul(np.linalg.inv(A), b[:,1])
+        x3 = np.matmul(np.linalg.inv(A), b[:,2])
         
-        self.alpha = np.zeros((x2.shape, 8, N))
+        self.alpha = np.zeros((8, N, 3))
 
-        print(self.alpha.shape)
-        self.alpha[:,:,1] = np.reshape(x1, 8, N)
-        self.alpha[:,:,2] = np.reshape(x2, 8, N)
-        self.alpha[:,:,3] = np.reshape(x3, 8, N)
+        self.alpha[:,:,0] = np.reshape(x1, (8, N), 'F')
+        self.alpha[:,:,1] = np.reshape(x2, (8, N), 'F')
+        self.alpha[:,:,2] = np.reshape(x3, (8, N), 'F')
+
+
+    def compute(self, t, state):
+        if t > self.traj_time[-1]:
+            t = self.traj_time[-1]
+
+        t_index = 0
+        for i in range(0, len(self.traj_time)):
+            if self.traj_time[i] >= t:
+                t_index = i
+                break
+
+        if t_index > 0:
+            t = t - self.traj_time[t_index-1]
+
+
+        desired_state = DesiredState()
+
+        if t == 0:
+            desired_state.pos = self.waypoints0[:,0]
+            desired_state.vel = np.zeros((3,1))
+            desired_state.acc = np.zeros((3,1))
+        else:
+            scale = t/self.d0[t_index-1]
+
+            f_p = np.squeeze(self.alpha[:,t_index-1,:]).T * np.reshape(np.repeat(self.p_c[0,:], 3), (3,8), 'F')
+            f_p = np.flip(f_p, 1)
+            desired_state.pos = np.array( [[np.polyval(f_p[0,:], scale)],
+                                          [np.polyval(f_p[1,:], scale)],
+                                          [np.polyval(f_p[2,:], scale)]] )
+
+            f_v = np.squeeze(self.alpha[:,t_index-1,:]).T * np.reshape(np.repeat(self.p_c[1,:],3), (3,8), 'F')
+            f_v = np.flip(f_v[:,np.arange(1,8)], 1)
+            desired_state.vel = np.array( [[np.polyval(f_v[0,:], scale)],
+                                          [np.polyval(f_v[1,:], scale)],
+                                          [np.polyval(f_v[2,:], scale)]] ) / self.d0[t_index-1]
+
+            f_a = np.squeeze(self.alpha[:,t_index-1,:]).T * np.reshape(np.repeat(self.p_c[2,:],3), (3,8), 'F')
+            f_a = np.flip(f_a[:,np.arange(2,8)], 1)
+
+            desired_state.acc = np.array( [[np.polyval(f_a[0,:], scale)],
+                                           [np.polyval(f_a[1,:], scale)],
+                                           [np.polyval(f_a[2,:], scale)]] ) /self.d0[t_index-1]**2
+
+        desired_state.yaw = 0
+        desired_state.yawdot = 0
+
+
+        return desired_state
 
 
 
@@ -104,3 +159,17 @@ if __name__ == "__main__":
                             [0,    0,   0]]).T
 
     traj_generator = MinimumSnap(waypoints)
+
+
+    t = np.linspace(0,200,1000)
+    states = np.empty((0, 3))
+
+    state = traj_generator.compute(4.04, None)
+
+    for i in t:
+        state = np.reshape(traj_generator.compute(i, None).pos, (1,3))
+        states = np.append(states, state, 0)
+
+
+    plt.plot(t, states[:,:], "-")
+    plt.show()
